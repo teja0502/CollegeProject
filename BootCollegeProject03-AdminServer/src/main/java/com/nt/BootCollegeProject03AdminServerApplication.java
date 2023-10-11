f8dac7a9a5f49f0b8bd34818427ad8ea551a0395
teja <teja@gmail.com>
2023-10-11 12:19:05 +0530

    Initial projects is committing to master branch

---
+++ BootCollegeProject03-AdminServer/src/main/java/com/nt/BootCollegeProject03AdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCollegeProject03AdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCollegeProject03AdminServerApplication.class, args);
+	}
+
+}
